0448388fd78b3fde30ea466b718377f076083bc3
DmitriyM <dmak1984@gmail.com>
2023-07-16 19:02:21 +0300

    Classifier is learned and pickled. Function module added.

---
+++ usage_sample.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Here is an example of using 
+the product classifier function for Peredelano-prices
+"""
+
+def main():
+    
+    from peredelano_classifier_v0 import pp_classifier
+
+    print(pp_classifier('Francuski makaronsi 24 kom 288g Chateau'))
+    
+if __name__ == '__main__':
+    main()
